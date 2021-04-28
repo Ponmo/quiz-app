31fe24910728e3de77eadac5543ce6edd821d339
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-04-28 09:34:21 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,9 +1,10 @@
-
-window.addEventListener( "pageshow", function ( event ) {
-  var historyTraversal = event.persisted || 
+$(document).ready(function() {
+  window.addEventListener( "pageshow", function ( event ) {
+    var historyTraversal = event.persisted || 
                          ( typeof window.performance != "undefined" && 
                               window.performance.navigation.type === 2 );
-  if ( historyTraversal ) {
-    window.location.reload();
-  }
+    if ( historyTraversal ) {
+      window.location.reload();
+    }
   });
+});
