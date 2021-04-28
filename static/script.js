99990e7e7ecabeeef25f63af646549a894db4d91
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-04-28 09:33:28 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,24 +1,9 @@
-jQuery(document).ready(function()
-        {
-                var d = new Date();
-                d = d.getTime();
-                if (jQuery('#reloadValue').val().length == 0)
-                {
-                        jQuery('#reloadValue').val(d);
-                        jQuery('body').show();
-                }
-                else
-                {
-                        jQuery('#reloadValue').val('');
-                        location.reload();
-                }
-        });
 
-#window.addEventListener( "pageshow", function ( event ) {
-#  var historyTraversal = event.persisted || 
-#                         ( typeof window.performance != "undefined" && 
-#                              window.performance.navigation.type === 2 );
-#  if ( historyTraversal ) {
-#    window.location.reload();
-#  }
-#  });
+window.addEventListener( "pageshow", function ( event ) {
+  var historyTraversal = event.persisted || 
+                         ( typeof window.performance != "undefined" && 
+                              window.performance.navigation.type === 2 );
+  if ( historyTraversal ) {
+    window.location.reload();
+  }
+  });
