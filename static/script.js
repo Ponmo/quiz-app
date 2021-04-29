faaa215f899972352dd0457932c5ec50ec07327f
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-04-28 21:02:01 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,8 +1,3 @@
 window.addEventListener( "pageshow", function ( event ) {
-  var historyTraversal = event.persisted || 
-                         ( typeof window.performance != "undefined" &&
-                          window.performance.navigation.type === 2 );
-  if ( historyTraversal ) {
     window.location.reload();
-  }
 });
